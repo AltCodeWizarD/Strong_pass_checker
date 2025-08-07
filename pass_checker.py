30681fcd85fda3496c395c9360241ff129b0a9ea
AltCodeWizarD <rusich2@gmail.com>
2025-08-07 16:25:17 +0300

    Исправил структуру кода
    Сделал код менее вложенным.
    Использовал .isalnum()
    Добавил ifmain

--- pass_checker.py
+++ pass_checker.py
@@ -1,30 +1,40 @@
-checking_password = input("Введите пароль для проверки:")
-pass_letters = list(checking_password)
-pass_length = len(checking_password)
-
-print(f"В пароле {pass_length} символов.")
+CHECKING_PASSWORD = input("Введите пароль для проверки: ")
 
 
 def pass_characteristics(pass_letters, pass_length):
     score = 0
-    print("Проверка надежности пароля: ")
-    digit_exist = any(letter.isdigit() for letter in pass_letters)
-    upper_exist = any(letter.isupper() for letter in pass_letters)
-    lower_exist = any(letter.islower() for letter in pass_letters)
-    symbols_exist = any(not (letter.isdigit() or letter.isalpha()) for letter in pass_letters)
-    if digit_exist:
-        score = score + 2
-        if pass_length >= 12:
-            score = score + 2
-            if upper_exist:
-                score = score + 2
-                if lower_exist:
-                    score = score + 2
-                    if symbols_exist:
-                        score = score + 2
-    else:
-        print("Пароль не соответствует параметрам")
+    print("Проверка надежности пароля:")
+
+    if not any(letter.isdigit() for letter in pass_letters):
+        return print("Пароль не содержит цифр")
+    score = score + 2
+
+    if pass_length < 12:
+        return print("Пароль слишком короткий")
+    score = score + 2
+
+    if not any(letter.isupper() for letter in pass_letters):
+        return print("Пароль не содержит заглавных букв")
+    score = score + 2
+
+    if not any(letter.islower() for letter in pass_letters):
+        return print("Пароль не содержит строчных букв")
+    score = score + 2
+
+    if all(letter.isalnum() for letter in pass_letters):
+        return print("Пароль не содержит специальных символов")
+    score = score + 2
+
     print(f"Надежность пароля: {score} очков")
+    return score
 
 
-pass_characteristics(pass_letters, pass_length)
+def main():
+    pass_letters = list(CHECKING_PASSWORD)
+    pass_length = len(CHECKING_PASSWORD)
+    print(f"В пароле {pass_length} символов.")
+    pass_characteristics(pass_letters, pass_length)
+
+
+if __name__ == '__main__':
+    main()
